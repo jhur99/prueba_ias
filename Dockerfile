dea849e7a3a00f8cb2050c2048ffc47929638b72
Jose Hurtado <jfhurtad@bancolombia.com.co>
2024-05-25 11:02:31 -0500

    project structure and consumption first update

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM node:20-alpine3.19
 WORKDIR /app
 COPY . .
 RUN npm install
-EXPOSE 3001
+EXPOSE 8787
 CMD ["npm", "start"]
 
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import express from 'express'
+import { dbinit } from './infrastructure/driven_adapters/database/dbinit/db.init'
+import { router } from './infrastructure/entry_points/routes/events.routes'
 
 const app = express()
 app.use(express.json())
 
-const PORT = process.env.PORT || 3001
+dbinit()
+
+const PORT = process.env.PORT || 8787
 
 app.listen(PORT, () =>{
     console.log(`escuchando por el puerto ${PORT}`)
 })
+
+app.use(router)
 
 app.get("/", (_req, res) => {
     res.send("Hola mundo")
